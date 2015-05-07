d7f08a27b16810bfb585ec235eb4184f21aaace2
Joshua Mun <joshmun23@gmail.com>
2015-05-06 20:51:07 -0400

    add feature to move pic from carousel container to featured pic, now need to slide over each pic to un-filled spot

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,24 +1,5 @@
-$('.carousel-item').click(function(e) {
-  prompt("works!");
-  // var firstItem = $('#first')[0].firstChild.nextSibling;
-  // var secondItem = $('#second')[0].firstChild.nextSibling;
-  // var thirdItem = $('#third')[0].firstChild.nextSibling;
-
-  // var firstSlot = $('#first')[0];
-  // var secondSlot = $('#first')[0];
-  // var thirdSlot = $('#first')[0];
-
-  var currentPic = e.target;
-
-    // newMenuItem = $.ajax({
-    //   url: '/users/' + currentUserID + '/menu_items/',
-    //   method: 'POST',
-    //   data: { currentUserID: currentUserID, restaurant: restaurant, menuItemID: menuItemID },
-    //   dataType: 'json'
-    // });
-  $('#feature-pic-container img').className="hi"
-  debugger;
-  // $('.featured-pic').add('<div class="small-12 large-4 columns featured-pic"><%= image_tag "kyoto.jpg" %></div>')
-  // $('.featured-pic')[0].ap(e.target);
-
+$('.carousel-container').on('click', '.carousel-item', function() {
+  currentPic = $(this).find('img');
+  currentPicID = $(this).attr('id');
+  $('#feature-pic-container').html(currentPic);
 });
