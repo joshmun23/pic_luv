462aa554ed24ebdbaa25b641108db9d81d6528c7
Joshua Mun <joshmun23@gmail.com>
2015-05-19 23:24:46 -0400

    add js + css to transform images, scale + rotate

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,15 +1,21 @@
 var $data = $.get('/tweets/new', function(response) {
   for (i = 0; i < response.length; i++){
-    currentSelector = '.carousel-container #' + i
-    tweets = $.parseJSON(response[i])
-    imgURL = tweets.photo_url[0]
+    currentSelector = '.carousel-container #' + i;
+    tweets = $.parseJSON(response[i]);
+    imgURL = tweets.photo_url[0];
 
     $(currentSelector).append('<li><img src=' + imgURL + '></li>');
+
+    $('#feature-pic-container').append('<img class=hidden src=' + imgURL + '>');
   };
 });
 
-$('.carousel-container').on('click', '.carousel-item', function() {
+$('.carousel-container div').on('mouseover', '.carousel-item img', function(e) {
+  e.preventDefault;
+  debugger;
   currentPic = $(this).find('img');
   currentPicID = $(this).attr('id');
-  $('#feature-pic-container').html(currentPic).fadeIn('slow');
+  $('#feature-pic-container').removeClass('hidden');
+  $('#feature-pic-container').addClass('hidden');
+
 });
