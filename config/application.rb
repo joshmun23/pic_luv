8fd8f80c050ae06f8d9227461533fd970b66495b
Joshua Mun <joshmun23@gmail.com>
2015-06-09 17:06:18 -0400

    save buttons css, update gemfile, add assets version to application to serve assets on production

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module PicLuv
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.compile = true
+    config.assets.version = '1.1'
   end
 end
